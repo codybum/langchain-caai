3817b6f904df2c65e4035cde42ca463ad4cfecfc
codybum <cbumgardner@gmail.com>
2024-06-24 08:00:04 -0400

    push updates

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='langchain-caai',
-    version='0.01',
+    version='0.4',
     packages=['langchain_caai'],
     url='https://caai.ai.uky.edu',
     license='Apache 2.0',
     author='CAAI',
     author_email='ai@uky.edu',
     description='Python Langchain CAAI Tools',
-    install_requires=['langchain_core'],
+    install_requires=['langchain_core','tqdm'],
 
 )
+
